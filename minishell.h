0a22d2c955bbda19f12540950ad8870d9703e667
Valentin Simeonov <vsimeono@c2s14d73.42wolfsburg.de>
2022-05-26 11:37:35 +0200

    Here we Go

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsimeono <vsimeono@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 11:34:07 by vsimeono          #+#    #+#             */
+/*   Updated: 2022/05/26 11:37:11 by vsimeono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>  /// TO DO - DELETE?
+
+
+
+#endif
