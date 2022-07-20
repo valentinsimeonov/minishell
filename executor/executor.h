0608b08d14835232704f0e5da363a564e53860c7
Saskia Mischnick <smischni@student.42wolfsburg.de>
2022-07-20 18:16:45 +0200

    .h updated

--- executor/executor.h
+++ executor/executor.h
@@ -6,7 +6,7 @@
 /*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 11:37:06 by smischni          #+#    #+#             */
-/*   Updated: 2022/07/20 17:01:29 by smischni         ###   ########.fr       */
+/*   Updated: 2022/07/20 18:16:18 by smischni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 int	executor(t_parser *parser);
 int	exec_section(t_list *sec, t_parser *parser);
 int	exec_prep(t_list *sec, t_parser *parser);
+int	create_command_array(int count, t_parser *parser, t_list *sec);
 
+int	is_infile(char *line);
 int	infile(char *file, t_list *sec, t_parser *parser, char *filemode);
+int	here_doc(char *lim);
+int	open_infile(char *filename);
 
+int	is_outfile(char *line);
 int	outfile(char *file, t_list *sec, t_parser *parser, char *filemode);
 
 #endif
