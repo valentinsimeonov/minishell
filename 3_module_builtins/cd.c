6ab6db38c911316b849910f94bfd93ae9e082acc
Saskia Mischnick <smischni@student.42wolfsburg.de>
2022-08-02 18:29:18 +0200

    Some builtin progress

--- 3_module_builtins/cd.c
+++ 3_module_builtins/cd.c
@@ -6,33 +6,38 @@
 /*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:59:34 by smischni          #+#    #+#             */
-/*   Updated: 2022/08/02 16:36:31 by smischni         ###   ########.fr       */
+/*   Updated: 2022/08/02 18:27:40 by smischni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	ft_cd(t_env *env, t_parser *parser)
+int	ft_cd(t_env *env, t_parser *parser)//maybe to export first?? -> use for adding to env list
 {
+	t_env	*tmp;
+	
 	if (!parser->command[1])
 	{
-		while (env)
-		{
-			if (ft_strncmp(env->bash_variable, "HOME", 5) == 0)
-			{
-				if (chdir(env->bash_v_content) < 0)
-					return (0);//error handling, no such file or directory
-				break;
-			}
-			else
-				env = env->next;
-		}
+		tmp = get_env(env, "HOME")
+		if (chdir(tmp->bash_v_content) < 0)
+			return (0);//error handling, no such file or directory
 	}
 	else
 	{
 		if (chdir(parser->command[1]) < 0)
 			return (0);//error handling, no such file or directory
 	}
-	//CHANGE PWD && OLD_PWD IN ENV!!!!!
+	tmp = get_env(env, "OLDPWD");
+	if (!tmp)
+		//add to env list -> ASK VALENTIN IF THERE IS A FUNCTION
+	else
+	{
+		free(tmp->bash_v_content);
+		tmp->bash_v_content = get_env(env, "PWD")->bash_v_content;
+	}
+	tmp = get_env(env, "PWD");
+	tmp->bash_v_content = ft_calloc(PATH_MAX, sizeof(char));
+	if (!getcwd(tmp->bash_v_content, PATH_MAX))
+		return (0);//error handling tbd
 	return (1);
 }
